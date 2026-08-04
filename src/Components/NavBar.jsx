d3704a76deb649badce9bea79e39894d7534a757
Silent1killer <planetearth68u@gmail.com>
2026-08-04 23:48:38 +0530

    vz

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,7 +3,7 @@ import NikeLogo from "../img/Logo/Nikelogo.svg"
 
 const NavBar = () => {
   return (
-    <div className="w-full flex justify-between my-7 bg-white p-5">
+    <div className="w-full flex justify-between my-7 bg-white p-5 sticky top-0 left-0 right-0 z-50 shadow-md">
       <div>
         <img src={NikeLogo} width='' height='' alt="Nike Logo" className="w-20 h-10" />
       </div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Banner from './HomeDetail/Banner'
+import Trending from './HomeDetail/Trending'
+import Fresh from './HomeDetail/Fresh'
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <Trending />
+      <Fresh />
     </div>
   )
 }
