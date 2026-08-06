c9714edf008a9778e2bbe3e856eef8cb8a8bb073
Silent1killer <planetearth68u@gmail.com>
2026-08-06 22:58:33 +0530

    dbv

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,15 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import NikeLogo from "../img/Logo/Nikelogo.svg";
 import { Link } from "react-router-dom";
-import { a } from "motion/react-client";
 
 const NavBar = () => {
-  const handleNavHover = (show) => {
-    const navHover = document.querySelector(".nav-hover");
-    if (navHover) {
-      navHover.style.display = show ? "flex" : "none";
-    }
-  };
+  const [showNavHover, setShowNavHover] = useState(false);
 
   return (
     <div className="relative">
@@ -17,21 +11,57 @@ const NavBar = () => {
         <Link to="/">
           <img
             src={NikeLogo}
-            width=""
-            height=""
             alt="Nike Logo"
             className="w-16 h-10"
           />
         </Link>
-        <nav className="flex gap-5 list-none text-md font-semibold">
-          <Link
-            to="/newAndFeatured"
-            className="newfeature"
-            onMouseEnter={() => handleNavHover(true)}
-            onMouseLeave={() => handleNavHover(false)}
+        <nav className="flex gap-5 list-none text-md font-semibold items-center">
+          <div
+            className="relative"
+            onMouseEnter={() => setShowNavHover(true)}
+            onMouseLeave={() => setShowNavHover(false)}
           >
-            New & Featured
-          </Link>
+            <Link to="/newAndFeatured" className="newfeature">
+              New & Featured
+            </Link>
+            <div
+              className={`nav-hover transition duration-300 ease-in-out transform ${
+                showNavHover ? "flex" : "hidden"
+              }`}
+              onMouseEnter={() => setShowNavHover(true)}
+              onMouseLeave={() => setShowNavHover(false)}
+            >
+              <div className="flex flex-col">
+                <h2>Featured</h2>
+                <a href="#!">New Arrivals</a>
+                <a href="#!">Bestsellers</a>
+                <a href="#!">Top Picks Under ₹4999</a>
+              </div>
+              <div className="flex flex-col">
+                <h2>Trending</h2>
+                <a href="#!">Retro Running</a>
+                <a href="#!">Just Do The Work</a>
+                <a href="#!">All Conditions Gear</a>
+              </div>
+              <div className="flex flex-col">
+                <h2>Shop Icons</h2>
+                <a href="#!">Air Force 1</a>
+                <a href="#!">Air Jordan 1</a>
+                <a href="#!">Air Max</a>
+                <a href="#!">Dunk</a>
+                <a href="#!">Pegasus</a>
+                <a href="#!">Vomero</a>
+              </div>
+              <div className="flex flex-col">
+                <h2>Sports</h2>
+                <a href="#!">Running</a>
+                <a href="#!">Gym & Training</a>
+                <a href="#!">Sportswear</a>
+                <a href="#!">Football</a>
+                <a href="#!">Basketball</a>
+              </div>
+            </div>
+          </div>
           <Link to="/men">Men</Link>
           <Link to="/women">Women</Link>
           <Link to="/kids">Kids</Link>
@@ -46,41 +76,6 @@ const NavBar = () => {
           />
           <p>kvn</p>
           <p></p>
-        </div>
-      </div>
-      <div
-        className="h-50 z-5 flex justify-between px-80 nav-hover transition duration-300 ease-in-out transform hover:scale-105"
-        onMouseEnter={() => handleNavHover(true)}
-        onMouseLeave={() => handleNavHover(false)}
-      >
-        <div className="flex flex-col">
-          <h2>Featured</h2>
-          <a href="">New Arrivals</a>
-          <a href="">Bestsellers</a>
-          <a href="">Top Picks Under ₹4999</a>
-        </div>
-        <div className="flex flex-col">
-          <h2>Trending</h2>
-          <a href="">Retro Running</a>
-          <a href="">Just Do The Work</a>
-          <a href="">All Conditions Gear</a>
-        </div>
-        <div className="flex flex-col">
-          <h2>Shop Icons </h2>
-          <a href="">Air Force 1</a>
-          <a href="">Air Jordan 1</a>
-          <a href="">Air Max</a>
-          <a href="">Dunk</a>
-          <a href="">Pegasus</a>
-          <a href="">Vomero</a>
-        </div>
-        <div className="flex flex-col">
-          <h2>Sports </h2>
-          <a href="">Running</a>
-          <a href="">Gym & Training</a>
-          <a href="">Sportswear</a>
-          <a href="">Football</a>
-          <a href="">Basketball</a>
         </div>
       </div>
     </div>
